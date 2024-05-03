6af0a4c05236c0479ec61b5ea24c4891d5c3b114
Corey Frenette <lordmonoxide@gmail.com>
2024-05-03 15:34:33 -0300

    Yesterday; formatting

--- public/index.php
+++ public/index.php
@@ -8,6 +8,10 @@ use Weather\Ui\Sensor;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$format = static function(string $result): string {
+  return sprintf("%.02f", $result);
+};
+
 $db = new Medoo([
   'type' => 'mysql',
   'host' => 'localhost',
@@ -20,8 +24,14 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function(Request $request, Response $response, $args) use($db) {
-  $results = $db->query('SELECT sensor, MIN(reading) min, MAX(reading) max FROM readings WHERE DATE(`timestamp`) = CURDATE() GROUP BY sensor')->fetchAll(PDO::FETCH_ASSOC);
+$app->get('/', function(Request $request, Response $response, $args) use($db, $format) {
+  $today = $db->query('SELECT sensor, MIN(reading) min, MAX(reading) max FROM readings WHERE DATE(`timestamp`) = CURDATE() GROUP BY sensor')->fetchAll(PDO::FETCH_ASSOC);
+  $yesterdayResults = $db->query('SELECT sensor, MIN(reading) min, MAX(reading) max FROM readings WHERE DATE(`timestamp`) = CURDATE() - INTERVAL 1 DAY GROUP BY sensor')->fetchAll(PDO::FETCH_ASSOC);
+
+  $yesterday = [];
+  foreach($yesterdayResults as $result) {
+    $yesterday[$result['sensor']] = $result;
+  }
 
   $currentReadings = [];
   foreach(Sensor::members() as $sensor) {
@@ -32,14 +42,21 @@ $app->get('/', function(Request $request, Response $response, $args) use($db) {
   }
 
   $formatted = '';
-  foreach($results as $result) {
+  foreach($today as $result) {
     if($currentReadings[$result['sensor']]) {
       $current = $currentReadings[$result['sensor']]['reading'];
     } else {
       $current = 'No reading';
     }
 
-    $formatted .= "<tr><td>{$result['sensor']}</></td><td>{$current}</td><td>{$result['max']}</td><td>{$result['min']}</td></tr>";
+    $formatted .= "<tr>
+      <td>{$result['sensor']}</td>
+      <td>{$format($current)}</td>
+      <td class='high'>{$format($result['max'])}</td>
+      <td class='low'>{$format($result['min'])}</td>
+      <td class='high'>{$format($yesterday[$result['sensor']]['max'])}</td>
+      <td class='low'>{$format($yesterday[$result['sensor']]['max'])}</td>
+    </tr>";
   }
 
   $output = <<<DOC
@@ -50,8 +67,13 @@ $app->get('/', function(Request $request, Response $response, $args) use($db) {
     <title>Weather</title>
     <meta http-equiv="refresh" content="30">
     <style>
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+      }
+    
       td {
-        padding-right: 1em;
+        padding-left: 0.5em;
+        padding-right: 0.5em;
       }
       
       thead td {
@@ -61,6 +83,34 @@ $app->get('/', function(Request $request, Response $response, $args) use($db) {
       tr:nth-child(even) {
         background-color: #f2f2f2;
       }
+      
+      td:not(:first-child) {
+        text-align: right;
+      }
+      
+      thead td.high {
+        background-color: #f09090;
+      }
+      
+      thead td.low {
+        background-color: #9090f0;
+      }
+      
+      tbody td.high {
+        background-color: #ffe0e0;
+      }
+      
+      tbody tr:nth-child(even) td.high {
+        background-color: #ffd0d0;
+      }
+      
+      tbody td.low {
+        background-color: #e0e0ff;
+      }
+      
+      tbody tr:nth-child(even) td.low {
+        background-color: #d0d0ff;
+      }
     </style>
   </head>
   <body>
@@ -69,8 +119,10 @@ $app->get('/', function(Request $request, Response $response, $args) use($db) {
         <tr>
           <td><strong>Sensor</strong></td>
           <td><strong>Current</strong></td>
-          <td><strong>Today's High</strong></td>
-          <td><strong>Today's Low</strong></td>
+          <td class="high"><strong>Today's High</strong></td>
+          <td class="low"><strong>Today's Low</strong></td>
+          <td class="high"><strong>Yesterday's High</strong></td>
+          <td class="low"><strong>Yesterday's Low</strong></td>
         </tr>
       </thead>
       <tbody>
